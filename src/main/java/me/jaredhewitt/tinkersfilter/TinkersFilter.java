4c7aecf8a170784a516733d5034c5afa570c0abc
Jared Hewitt <jhewitt@invoca.com>
2020-07-12 03:35:08 -0400

    This adds config options for filtering tools and patterns, as well as the initial logic for performing those tasks.

--- src/main/java/me/jaredhewitt/tinkersfilter/TinkersFilter.java
+++ src/main/java/me/jaredhewitt/tinkersfilter/TinkersFilter.java
@@ -1,31 +1,34 @@
 package me.jaredhewitt.tinkersfilter;
 
-import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import me.jaredhewitt.tinkersfilter.Config;
+
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = TinkersFilter.MODID, name = TinkersFilter.NAME, version = TinkersFilter.VERSION)
 public class TinkersFilter
 {
-    public static final String MODID = "tinkersfilter";
-    public static final String NAME = "Tinkers' Filter";
-    public static final String VERSION = "${version}";
+  public static final String MODID = "tinkersfilter";
+  public static final String NAME = "Tinkers' Filter";
+  public static final String VERSION = "${version}";
 
-    private static Logger logger;
+  private static Logger logger;
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
-    }
+  @EventHandler
+  public void preInit(FMLPreInitializationEvent event)
+  {
+    MinecraftForge.EVENT_BUS.register(new EventManager());
+    Config.load(event);
+    logger = event.getModLog();
+  }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("Hello World");
-    }
+  @EventHandler
+  public void init(FMLInitializationEvent event)
+  {
+    logger.info("Starting " + NAME);
+  }
 }
